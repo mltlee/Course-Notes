b707f9bd1ab9463d9048fb47d18d3343a8288686
mltlee <mlee21592@gmail.com>
2021-10-02 16:37:24 -0400

    add details option to scripts

--- CO 485/Algorithms/IntegerFactorization.py
+++ CO 485/Algorithms/IntegerFactorization.py
@@ -1,12 +1,17 @@
 import GCD
 
 # N is assumed odd
-def Pollard(N, B):
+def Pollard(N, B, details = False):
     a = 2
+    if details: 
+        print("N = {}, B = {}, a = {}".format(N, B, a))
     for i in range(2, B+1):
         a = pow(a, i, N)
     n = GCD.GCD(a - 1, N)
+    if details:
+        print("a^(B!) (mod N) = {}".format(a))
+        print("n = gcd(a^(B!) - 1, N) = {}".format(n))
     if n > 1 and n < N:
-        return "n = {} is a nontrivial factor of N = {}".format(n, N)
+        print("n = {} is a nontrivial factor of N = {}".format(n, N))
     else:
-        return "failed to find a nontrivial factor of N = {}".format(N)
+        print("Failed to find a nontrivial factor of N = {}".format(N))
--- CO 485/Algorithms/PrimalityTests.py
+++ CO 485/Algorithms/PrimalityTests.py
@@ -27,7 +27,7 @@ def SolovayStrassen(n, r):
                 return "n = {} is composite: Euler-Jacobi witness a = {} for the compositeness of n = {}".format(n, a, n)
     return "n = {} is probably prime: no Euler-Jacobi witness found in r = {} iterations".format(n, r)
 
-def MillerRabin(n, r):
+def MillerRabin(n, r, details = False):
     d = n - 1
     s = 0
     while d % 2 == 0:
@@ -35,20 +35,45 @@ def MillerRabin(n, r):
             break
         d = int(d / 2)
         s = s + 1
+    if details:
+        print()
+        print("Input n = {}, r = {}".format(n, r))
+        print("n - 1 = 2^s * d where s = {}, d = {}".format(s, d))
     for i in range(r):
         a = random.randint(2, n-2)
+        if details:
+            print()
+            print("Iteration {}: a = {} selected".format(i+1, a))
+            print("---------------------------------")
         gcd = GCD.GCD(a, n)
         if gcd > 1:
-            return "n = {} is composite: gcd(a, n) > 1 where a = {} and n = {}".format(n, a, n)
+            print("n = {} is composite: gcd(a, n) > 1 where a = {} and n = {}".format(n, a, n))
+            return 
         else:
             b = pow(a, d, n)
+            if details: 
+                print("b = a^d (mod n) = {}".format(b))
             if b == 1:
+                if details:
+                    print("b = {} is congruent to 1 mod n, so a = {} is not a Miller-Rabin witness".format(b, a))
                 continue
+            if details:
+                print("b = {} is not congruent to 1 mod n".format(b))
+                print("Computing b^(2^t) for 0 <= t <= s-1:")
             count = 0
             for t in range(s):
                 power = pow(b, 2**t, n)
-                if power == -1 % n:
+                if details:
+                    print("b^(2^{}) (mod n) = {}".format(t, power))
+                if power == -1 % n and count == 0:
                     count = count + 1
-            if count == 0:
-                return "n = {} is composite: Miller-Rabin witness a = {} for the compositeness of n = {}".format(n, a, n)
-    return "n = {} is probably prime: no Miller-Rabin witness found in r = {} iterations".format(n, r)
+                    if details:
+                        print("b^(2^{}) is congruent to -1 mod n, so a = {} is not a Miller-Rabin witness".format(t, a))
+                    continue 
+                if details:
+                    print("None of these are congruent to -1 mod n")
+                print()
+                print("n = {} is composite: Miller-Rabin witness a = {} for the compositeness of n = {}".format(n, a, n))
+                return 
+    print()
+    print("n = {} is probably prime: no Miller-Rabin witness found in r = {} iterations".format(n, r))
